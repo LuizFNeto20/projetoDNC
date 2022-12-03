15221d08cfa4b699053b75df6a903bb8410cd1c0
Luiz Ferreira <luizfneto201120@gmail.com>
2022-12-03 11:57:42 -0400

    Projeto DNC - incompleto

---
+++ assets/script/script.js
@@ -0,0 +1,11 @@
+function linkCompras() {
+    window.location.href = 'Compras.html'
+}
+
+function linkCompras2() {
+    window.location.href = 'Compras2.html'
+}
+
+function finalCompra() {
+    window.location.href = 'final.html'
+}
